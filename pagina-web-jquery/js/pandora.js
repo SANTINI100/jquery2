ce6dc0b318e8270c5de95e56426e312d6ff968b7
SANTINI100 <jspiraquive9@misena.edu.co>
2021-08-19 12:58:56 -0500

    creacion del primer commit (carga de 3 carpetas)

---
+++ pagina-web-jquery/js/pandora.js
@@ -0,0 +1,3 @@
+var contents = prompt("Que quieres colocar en la caja de pandora ?", "Ingresa aqui lo deseado");
+console.log("colocaste " + contents + " en la caja de pandora");
+$('#pandora').html(contents);
